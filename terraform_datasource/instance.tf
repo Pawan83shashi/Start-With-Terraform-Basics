2718f161c9fcb4046f0f85b792d1791e3d349113
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 00:31:47 +0530

    done

--- terraform_datasource/instance.tf
+++ terraform_datasource/instance.tf
@@ -4,7 +4,7 @@
 resource "aws_instance" "Frontend" {
     ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type="t2.micro"
-    key_name = aws_key_pair.levelup_key.key_name
+    
 
 
   tags = {
