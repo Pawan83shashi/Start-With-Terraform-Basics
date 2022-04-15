0e3827ec6ba4523c150c38ddb1a57ff0a328ed59
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 01:10:56 +0530

    added random ami

--- terraform_datasource/instance.tf
+++ terraform_datasource/instance.tf
@@ -1,8 +1,21 @@
 data "aws_availability_zones" "available" {}
+data "aws_ami" "latest-ami" {
+  most_recent = true
+  owner = ["amazon"]
+  filter {
+    name = "name"
+    values = "amzn2-ami-hvm-2*"
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
 
 # Create Fronted Server
 resource "aws_instance" "Frontend" {
-    ami = lookup(var.AMIS, var.AWS_REGION)
+    ami = data.aws_ami.latest-ami.id
     instance_type="t2.micro"
     availability_zone = data.aws_availability_zones.available.names[1]
 
