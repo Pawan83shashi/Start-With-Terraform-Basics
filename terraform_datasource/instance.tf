9ec6a1186a6c6dc26e0cf15ff74e1d72415b90a9
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 00:37:46 +0530

    added availability zone selection

--- terraform_datasource/instance.tf
+++ terraform_datasource/instance.tf
@@ -1,10 +1,10 @@
-# Create AWS Key Pair
+data "aws_availability_zones" "available" {}
 
 # Create Fronted Server
 resource "aws_instance" "Frontend" {
     ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type="t2.micro"
-    
+    availability_zone = data.aws_availability_zones.available.names[1]
 
 
   tags = {
