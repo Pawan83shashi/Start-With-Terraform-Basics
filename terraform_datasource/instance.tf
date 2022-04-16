7f18e62062f17d0e2a81071101544e0e3eec064f
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 18:23:00 +0530

    wetwtd

--- terraform_datasource/instance.tf
+++ terraform_datasource/instance.tf
@@ -3,17 +3,24 @@ data "aws_availability_zones" "available" {}
 data "aws_ami" "latest" {
   most_recent = true
   owners = ["amazon"]
-   
-  image=`aws ssm get-parameters-by-path --path /aws/service/ami-amazon-linux-latest --query "Parameters[].Value"`
 
-  }
+      filter {
+        name   = "root-device-type"
+        values = ["ebs"]
+      }
+
+      filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+      }
+}
 
   
 
 
 # Create Fronted Server
 resource "aws_instance" "Frontend" {
-    ami = data.aws_ami.latest.image
+    ami = data.aws_ami.latest.id
     instance_type="t2.micro"
     availability_zone = data.aws_availability_zones.available.names[1]
 
