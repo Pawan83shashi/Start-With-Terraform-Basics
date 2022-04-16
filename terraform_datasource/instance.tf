b6481eabcb093097873f2e6e6a4fdc0d5d8bdb3d
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 23:09:00 +0530

    change done

--- terraform_datasource/instance.tf
+++ terraform_datasource/instance.tf
@@ -1,31 +1,8 @@
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "latest" {
-  most_recent = true
-  owners = ["amazon"]
-
-      filter {
-        name = "name"
-        values = ["/aws/service/ami-amazon-linux-latest"]
-      }
-      
-      filter {
-        name   = "root-device-type"
-        values = ["ebs"]
-      }
-
-      filter {
-        name   = "virtualization-type"
-        values = ["hvm"]
-      }
-}
-
-  
-
-
 # Create Fronted Server
 resource "aws_instance" "Frontend" {
-    ami = data.aws_ami.latest.id
+    ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type="t2.micro"
     availability_zone = data.aws_availability_zones.available.names[1]
 
