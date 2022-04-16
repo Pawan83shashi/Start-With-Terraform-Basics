1fc6f39bf6eaaf2712e6a6647998b76ae1b93ff5
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-16 23:25:57 +0530

    named func tag as tags

--- terraform_datasource/securitygroup.tf
+++ terraform_datasource/securitygroup.tf
@@ -13,7 +13,7 @@ resource "aws_security_group" "sg-custom_us_east" {
         cidr_blocks = data.aws_ip_ranges.us_east_ip_range.cidr_blocks
     }
 
-    tag = {
+    tags = {
         CreateDate = data.aws_ip_ranges.us_east_ip_range.create_date
         SyncToken  =data.aws_ip_ranges.us_east_ip_range.sync_token
     }
