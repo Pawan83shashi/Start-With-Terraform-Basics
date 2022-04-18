05f243d003a532da469dc76039ca920e15df78c1
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-18 12:53:35 +0530

    correct syntax in vpc.tx

--- Terraform_For_AWS_Cloud/VPC_NAT_EC2/vpc.tf
+++ Terraform_For_AWS_Cloud/VPC_NAT_EC2/vpc.tf
@@ -3,10 +3,10 @@ resource "aws_vpc" "levelup_vpc" {
     cidr_block = "10.0.0.0/16"
     instance_tenancy = "default"
     enable_dns_support = "true"
-    enable_dns_hostname = "true"
+    enable_dns_hostnames = "true"
     enable_classiclink = "false"
     
-    tag = {
+    tags = {
         Name = "Main"
     }
 }
