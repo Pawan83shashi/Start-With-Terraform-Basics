185baf8ce84800deaeffb8719a367f28e16c852d
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-18 13:23:54 +0530

    corrected sysntax

--- Terraform_For_AWS_Cloud/VPC_NAT_EC2/instance.tf
+++ Terraform_For_AWS_Cloud/VPC_NAT_EC2/instance.tf
@@ -9,7 +9,7 @@ resource "aws_instance" "Frontend" {
     ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type="t2.micro"
     key_name = aws_key_pair.newlevelup_key.key_name
-    vpc_security_group_ids = [aws_security_group.allow-level-ssh.id]
+    vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
     subnet_id = aws_subnet.levelupvpc-public-1.id
 
   tags = {
