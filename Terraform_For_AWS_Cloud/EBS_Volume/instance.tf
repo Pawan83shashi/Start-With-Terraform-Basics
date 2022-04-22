5edc39acca500b9748e389a9f8d6cfeb907a4001
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-22 10:16:55 +0530

    remove security group } in EBS block

--- Terraform_For_AWS_Cloud/EBS_Volume/instance.tf
+++ Terraform_For_AWS_Cloud/EBS_Volume/instance.tf
@@ -9,9 +9,7 @@ resource "aws_instance" "Frontend-EBS" {
     ami = lookup(var.AMIS, var.AWS_REGION)
     instance_type="t2.micro"
     key_name = aws_key_pair.newlevelup_key.key_name
-    vpc_security_group_ids = [aws_security_group.allow-levelup-ssh.id]
-    subnet_id = aws_subnet.levelupvpc-public-1.id
-
+    
   tags = {
     Name = "Frontend_Server"
     Project = "BMT"
