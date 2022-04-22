92d9426b25caff6816fac6dd70da8ec40b386910
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-22 10:21:41 +0530

    remove security group } in EBS block

--- Terraform_For_AWS_Cloud/EBS_Volume/instance.tf
+++ Terraform_For_AWS_Cloud/EBS_Volume/instance.tf
@@ -33,5 +33,5 @@ resource "aws_ebs_volume" "ebs-volume-1" {
   resource "aws_volume_attachment" "ebs-volume-1-attachment" {
   device_name = "/dev/xvdh"
   volume_id   = aws_ebs_volume.ebs-volume-1.id
-  instance_id = aws_instance.Fronted-EBS.id
+  instance_id = aws_instance.Frontend-EBS.id
 }
