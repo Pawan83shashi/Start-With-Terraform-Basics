576dad3e161456f3c678b9dbc3174cc07c47e328
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-23 13:20:37 +0530

    User data script added

--- Terraform_For_AWS_Cloud/User_Data/instance.tf
+++ Terraform_For_AWS_Cloud/User_Data/instance.tf
@@ -19,22 +19,3 @@ resource "aws_instance" "Frontend-EBS" {
   }
  
 }
-
-# EBS Resource Creation
-resource "aws_ebs_volume" "ebs-volume-1" {
-  availability_zone = "us-east-1a"
-  size              = 20
-  type              = "gp2"
-
-  tags = {
-    Name = "Secondary Volume Disk"
-  }
-}
-  # Attach EBS Volumme with AWS Instance
-
-  resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-  device_name = "/dev/xvdh"
-  volume_id   = aws_ebs_volume.ebs-volume-1.id
-  instance_id = aws_instance.Frontend-EBS.id
-}
-
