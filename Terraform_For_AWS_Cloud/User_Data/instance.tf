af6192363fa3e3866ec858408535cf6dbafd0a89
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-23 12:25:03 +0530

    User data script added

--- Terraform_For_AWS_Cloud/User_Data/instance.tf
+++ Terraform_For_AWS_Cloud/User_Data/instance.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "Frontend-EBS" {
     instance_type="t2.micro"
     key_name = aws_key_pair.newlevelup_key.key_name
     availability_zone = "us-east-1a"
-    user_date = "installapache.sh"
+    user_data = "installapache.sh"
     
   tags = {
     Name = "Frontend_Server"
