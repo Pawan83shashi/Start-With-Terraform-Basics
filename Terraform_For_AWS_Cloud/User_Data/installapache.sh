544906f299baf94a12ce17d67d4c5d847c3b1b55
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-04-23 14:45:40 +0530

    User data script added

--- Terraform_For_AWS_Cloud/User_Data/installapache.sh
+++ Terraform_For_AWS_Cloud/User_Data/installapache.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
-apt-get update
-apt-get install -y apache2
-systemctl start apache2
-systemctl enable apache2
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+    sleep 1
+done
+
+sudo yum update
+sudo yum install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
 echo "<h1>Deployed Machine via Terraform</h1>" | sudo tee /var/www/html/index.html
