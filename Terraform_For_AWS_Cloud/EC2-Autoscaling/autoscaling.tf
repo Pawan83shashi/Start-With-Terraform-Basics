81771b1534b0c959b4e5384d514cf00ea2989a48
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-05-09 10:20:17 +0530

    modified

--- Terraform_For_AWS_Cloud/EC2-Autoscaling/autoscaling.tf
+++ Terraform_For_AWS_Cloud/EC2-Autoscaling/autoscaling.tf
@@ -3,7 +3,7 @@ resource "aws_launch_configuration" "levelup-launchconfig" {
   name_prefix = "levelup-launchconfig"
   image_id = lookup(var.AMIS, var.AWS_REGION)
   instance_type = "t2.micro"
-  key_name = aws_key_pair.levelup_key.key_name
+  key_name = aws_key_pair.levelup_keyname
  
 }
 
