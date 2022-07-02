845d5f0b7a505641ea99e84b9ee0eb84ff807a28
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-07-02 18:22:07 +0530

    added install nginx

--- Packer/Deploy_Custom_Image/custom_ami_deployment/createinstance.tf
+++ Packer/Deploy_Custom_Image/custom_ami_deployment/createinstance.tf
@@ -1,7 +1,7 @@
 # Create Instance uisng Custom VPC
 
 module "develop-vpc" {
-    soruce = "../modules/vpc"
+    source = "../modules/vpc"
     ENVIRONMENT = var.ENVIRONMENT
     AWS_REGION = var.AWS_REGION
 }
