b0f810f7f8b180ef81c64df5df7a253c5eda1e7a
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-07-02 17:42:48 +0530

    packer and modules

---
+++ Packer/Deploy_Custom_Image/modules/variable.tf
@@ -0,0 +1,10 @@
+#Define Variable for Custom Module VPC
+variable "AWS_REGION" {
+    type    = string
+    default = "us-east-1"
+}
+
+variable "ENVIRONMENT" {
+    type    = string
+    default = ""
+}
