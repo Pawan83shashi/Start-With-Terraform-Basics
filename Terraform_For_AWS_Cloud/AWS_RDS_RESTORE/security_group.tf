b9f103f234c9439c38fd00023b77ec1b0da68b2f
Pawan Kumar <pawan83.shashi@yahoo.in>
2022-07-07 09:20:26 +0530

    Addes RDS restore codes

---
+++ Terraform_For_AWS_Cloud/AWS_RDS_RESTORE/security_group.tf
@@ -0,0 +1,53 @@
+# Security group for levelupvpc
+resource "aws_security_group" "allow-levelup-ssh" {
+  name        = "allow-levelup-ssh"
+  description = "Security group that allows ssh connection"
+  vpc_id      = aws_vpc.levelup_vpc.id
+
+  ingress {
+    
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  tags = {
+    Name = "allow-levelup-ssh"
+  }
+}
+
+# Security group for MariaDB
+resource "aws_security_group" "allow-mariadb" {
+  name        = "allow-mariadb"
+  description = "Security group that allows mariadb"
+  vpc_id      = aws_vpc.levelup_vpc.id
+
+  ingress {
+    
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    security_groups = [aws_security_group.allow-levelup-ssh.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  tags = {
+    Name = "allow-mariadb"
+  }
+}
